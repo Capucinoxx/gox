fad0028626ad784d15b71232c6d19984577c7d38
Capucinoxx <bendertermebc@gmail.com>
2021-12-16 21:48:07 -0500

    [feat] - fonction de comparaison de rune pour lexer

---
+++ pkg/lexer/lexer.go
@@ -0,0 +1,11 @@
+package lexer
+
+var eof = rune(0)
+
+func isWhitespace(ch rune) bool {
+	return ch == ' ' || ch == '\t' || ch == '\n'
+}
+
+func isLetter(ch rune) bool {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+}
