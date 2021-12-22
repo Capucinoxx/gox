8518db08b9e6b6ec532c01c1bbb84066fb132e31
Capucinoxx <bendertermebc@gmail.com>
2021-12-22 01:44:01 -0500

    [fix] - correction représentation interface d'un nombre

--- pkg/object/object_number.go
+++ pkg/object/object_number.go
@@ -7,4 +7,4 @@ type Number struct{ Value float64 }
 
 func (i Number) Type() Type               { return OBJECT_NUMBER }
 func (i Number) ToString() string         { return fmt.Sprint(i.Value) }
-func (i Number) ToInterface() interface{} { return i.Value }
+func (i Number) ToInterface() interface{} { return i }
