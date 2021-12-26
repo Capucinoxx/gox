34af8f45b177f23d207f5c6f6f4b4ca77a19cf5e
Capucinoxx <bendertermebc@gmail.com>
2021-12-26 15:38:19 -0500

    [feat] - soustraction dans un cannal

--- pkg/object/object_chan.go
+++ pkg/object/object_chan.go
@@ -2,17 +2,26 @@ package object
 
 type Chan struct{ ch chan func() Object }
 
-func (c *Chan) Make(tpe Object) {
+// Make initalisation d'un cannal de fonction permettant l'ajout d'une fonction
+// pour l'évaluation de l'expression
+func (c *Chan) Make() {
 	c.ch = make(chan func() Object, 1)
-
-	switch tpe.(type) {
-	case *Number:
-		c.ch <- func() Object { return Number{Value: 0} }
-	}
-
+	c.ch <- func() Object { return Null{} }
 }
+
+// Eval fait l'évaluation de la composition de fonction présente dans le cannal
+func (c *Chan) Eval() Object { return (<-c.ch)() }
+
+// Add ajoute une fonction d'addition de l'objet (Object.Add) à la composition
+// de fonction présente pour l'expression
 func (c *Chan) Add(obj Object) {
 	old := <-c.ch
 	c.ch <- func() Object { return old().Add(obj) }
 }
-func (c *Chan) Eval() Object { return (<-c.ch)() }
+
+// Sub ajoute une fonction de soustraction de l'objet (Object.Sub) à la
+// composition de fonction déjà présente pour l'expression
+func (c *Chan) Sub(obj Object) {
+	old := <-c.ch
+	c.ch <- func() Object { return old().Sub(obj) }
+}
