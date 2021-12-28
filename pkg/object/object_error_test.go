df905c8d7fcaefeb279571065483f27fc8bec22f
Capucinoxx <bendertermebc@gmail.com>
2021-12-28 17:53:43 -0500

    [feat] - gestion opération Soustraction sur une erreur

--- pkg/object/object_error_test.go
+++ pkg/object/object_error_test.go
@@ -57,3 +57,56 @@ func TestAddError(t *testing.T) {
 		})
 	}
 }
+
+func TestSubError(t *testing.T) {
+	cases := map[string]struct {
+		a       object.Error
+		b       object.Object
+		wantStr string
+	}{
+		"object.Error.Sub(object.String): object.Error": {
+			a:       object.Error{Error: "err random"},
+			b:       object.String{},
+			wantStr: "err random",
+		},
+		"object.Error.Sub(object.Number): object.Error": {
+			a:       object.Error{Error: "err random"},
+			b:       object.Number{},
+			wantStr: "err random",
+		},
+		"object.Error.Sub(object.Null): object.Error": {
+			a:       object.Error{Error: "err random"},
+			b:       object.Null{},
+			wantStr: "err random",
+		},
+		"object.Error.Sub(object.Array): object.Error": {
+			a: object.Error{Error: "err random 2"},
+			b: object.Array{
+				Elements: []object.Object{
+					object.Error{Error: "err random 1"},
+				},
+			},
+			wantStr: "err random 2",
+		},
+		"object.Error.Sub(object.Error): object.Error": {
+			a:       object.Error{Error: "err random 1"},
+			b:       object.Error{Error: "err random 2"},
+			wantStr: "err random 1",
+		},
+	}
+
+	for name, tt := range cases {
+		t.Run(name, func(t *testing.T) {
+			res := tt.a.Sub(tt.b)
+
+			if tt.wantStr != res.ToString() {
+				t.Errorf(
+					"%q addition mismatch: exp=%s got=%s",
+					tt.b,
+					res,
+					tt.wantStr,
+				)
+			}
+		})
+	}
+}
