745047a870d50a121c8b5a798f9fdf82b4ec9a85
Capucinoxx <bendertermebc@gmail.com>
2021-12-28 23:36:48 -0500

    [feat] - tests opération Addition sur dans un Channel

---
+++ pkg/object/object_chan_test.go
@@ -0,0 +1,54 @@
+package object_test
+
+import (
+	"github.com/Capucinoxx/gox/pkg/object"
+	"testing"
+)
+
+func TestChanAdd(t *testing.T) {
+	cases := map[string]struct {
+		loopTime int
+		value    object.Object
+		wantStr  string
+	}{
+		"object.Chan.Add(10) 20 times: object.Number": {
+			loopTime: 20,
+			value:    object.Number{Value: 10},
+			wantStr:  "200",
+		},
+		"object.Chan.Add('t') 10 times: oject.String": {
+			loopTime: 10,
+			value:    object.String{Value: "t"},
+			wantStr:  "tttttttttt",
+		},
+		"object.Chan.Add(object.Null) 10 times: oject.Null": {
+			loopTime: 10,
+			value:    object.Null{},
+			wantStr:  "null",
+		},
+	}
+
+	for name, tt := range cases {
+		t.Run(name, func(t *testing.T) {
+			ch := object.Chan{}
+			ch.Make()
+			ch.GenFunc("+")
+
+			for i := 0; i < tt.loopTime; i++ {
+				ch.Add(tt.value)
+			}
+
+			res := ch.Eval()
+
+			if res.ToString() != tt.wantStr {
+				t.Errorf(
+					"%q string mismatch: exp=%s got=%s",
+					res,
+					tt.wantStr,
+					res.ToString(),
+				)
+			}
+		})
+	}
+
+}
