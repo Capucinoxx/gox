abddbdd3d51ae426deeb751ae27afc2c9f744732
Capucinoxx <bendertermebc@gmail.com>
2021-12-29 13:02:27 -0500

    [feat] - gestion des opérations de Division et Multiplication sur les objet de type Null

--- pkg/object/object_null.go
+++ pkg/object/object_null.go
@@ -18,3 +18,7 @@ func (n Null) Add(oth Object) Object { return oth }
 // Sub retournes l'objet passé en paramètre
 // TODO: doit envoyé  0 - valeur si nombre
 func (n Null) Sub(oth Object) Object { return oth }
+
+func (n Null) Mul(oth Object) Object { return oth }
+
+func (n Null) Div(oth Object) Object { return oth }
